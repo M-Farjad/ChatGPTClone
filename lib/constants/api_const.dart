2885fc734a08aa31a02d1d219381f6177b4d687c
M-Farjad <2021SE56@student.uet.edu.pk>
2023-11-08 12:00:30 +0500

    UpdatedFiles

--- lib/constants/api_const.dart
+++ lib/constants/api_const.dart
@@ -1,2 +1,3 @@
 String BASE_URL = 'https://api.openai.com/v1';
-String API_KEY = "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";
+// String API_KEY = "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";
+String API_KEY = "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";
